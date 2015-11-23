86f81a328f787d4130ef318bc285137bb76ed045
Antoine Letreguilly <antoine.letreguilly@free.fr>
2015-11-23 10:31:09 +0100

    pop

---
+++ Mars_Mission_Control_Dev/Spationaute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PI_Mars_Mission_Control
+{
+	public class Spationaute
+	{
+		public Spationaute()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public string Nom
+		{
+			get
+			{
+				throw new System.NotImplementedException();
+			}
+			set
+			{
+			}
+		}
+
+		public List<Activite> afficherActiviteSpationaute()
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
