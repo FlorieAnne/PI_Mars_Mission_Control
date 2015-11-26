87ec6cd4c76815a08e008f648aa7edf52e6fc2a7
FlorieAnne <fmondoncanc@ensc.fr>
2015-11-26 17:41:17 +0100

    FOrm 2

--- Mars_Mission_Control_Dev/Form2.cs
+++ Mars_Mission_Control_Dev/Form2.cs
@@ -13,7 +13,9 @@ namespace PI_Mars_Mission_Control
     {
         private Journee jourActuel;
         private int _taille10minPixel;
-        private List<Activite> listActi = new List<Activite>();
+        private List<Activite> listActi = new List<Activite>(); // liste des activités
+        List<int> listTailles = new List<int>(); // taille en pixel d'une activité
+        List<int> listEcart = new List<int>(); // ecart entre 2 activités
 
         public Form2(int jour)
         {
@@ -21,8 +23,9 @@ namespace PI_Mars_Mission_Control
             jourActuel = Journee.ListeJournees.ElementAt(jour);
             this.tagJourActuel.Text = jour.ToString();
             this.tagJourActuel2.Text = jour.ToString();
-            _taille10minPixel = 10;
-            Activite Sleeping1 = new Activite(new Dates(0,0,jourActuel.NumJour), new Dates(7,0,jourActuel.NumJour), new Coordonnees(new Point(), pictureBox1.Image, "COUCOU"), "texte");
+            _taille10minPixel = 2; // 10 minutes = 10 pixel
+            // On ajoute toutes les activités par défaut
+            Activite Sleeping1 = new Activite(new Dates(0, 0, jourActuel.NumJour), new Dates(7, 0, jourActuel.NumJour), new Coordonnees(new Point(), pictureBox1.Image, "COUCOU"), "texte");
             Activite Sleeping2 = new Activite(new Dates(23, 0, jourActuel.NumJour), new Dates(24, 40, jourActuel.NumJour), new Coordonnees(new Point(), pictureBox1.Image, "COUCOU"), "texte");
             Activite Eating1 = new Activite(new Dates(7, 0, jourActuel.NumJour), new Dates(8, 0, jourActuel.NumJour), new Coordonnees(new Point(), pictureBox1.Image, "COUCOU"), "texte");
             Activite Eating2 = new Activite(new Dates(12, 0, jourActuel.NumJour), new Dates(14, 0, jourActuel.NumJour), new Coordonnees(new Point(), pictureBox1.Image, "COUCOU"), "texte");
@@ -39,12 +42,13 @@ namespace PI_Mars_Mission_Control
             listActi.Add(Private2);
             listActi.Add(Private3);
             listActi.Sort(
-                delegate(Activite a1, Activite a2)
+                delegate(Activite a1, Activite a2) // Et on les ordonne
                 {
-                    int HA1 = a1.HeureDebut.heure*60+a1.HeureDebut.minute;
-                    int HA2 = a2.HeureDebut.heure*60+a2.HeureDebut.minute;
+                    int HA1 = a1.HeureDebut.heure * 60 + a1.HeureDebut.minute;
+                    int HA2 = a2.HeureDebut.heure * 60 + a2.HeureDebut.minute;
                     return HA1.CompareTo(HA2);
                 });
+            ajusteNbLigne();
         }
 
         private void retourCalendrier_Click(object sender, EventArgs e)
@@ -98,5 +102,44 @@ namespace PI_Mars_Mission_Control
         {
             return ((Activitee.HeureFin.heure - Activitee.HeureDebut.heure) * 60 + (Activitee.HeureFin.minute - Activitee.HeureDebut.minute)) / 10 * _taille10minPixel;
         }
+
+        private void tailleChaqueCreneaux()
+        {
+            foreach (Activite i in listActi)
+            {
+                listTailles.Add(tailleActivite(i));
+            }
+
+            listEcart.Add(((listActi[0].HeureFin.heure * 60 + listActi[0].HeureFin.minute) - 0) / 10 * _taille10minPixel); // écart entre heure 0 et première activité
+
+            for (int j = 0; j < listActi.Count() - 1; j++)
+            {
+                listEcart.Add(((listActi[j + 1].HeureFin.heure * 60 + listActi[j + 1].HeureFin.minute) - (listActi[j].HeureFin.heure * 60 + listActi[j].HeureFin.minute)) / 10 * _taille10minPixel); // écart entre 2 activités
+            }
+
+            listEcart.Add(((24 * 60 + 40 - (listActi[listActi.Count - 1].HeureFin.heure * 60 + listActi[listActi.Count - 1].HeureFin.minute))) / 10 * _taille10minPixel); // écart entre dernière activité et 24h40
+        }
+
+        private int calculNbLigne()
+        {
+            IEnumerable<int> listEcartNonNul = listEcart.Where(p => p == 0); // enlève les valeurs nulles de la liste
+            int nbEcartNonNul = listEcartNonNul.Count();
+            return listTailles.Count + nbEcartNonNul;
+        }
+
+        private void ajusteNbLigne()
+        {
+            tailleChaqueCreneaux();
+            tableLayoutPanel1.RowCount = calculNbLigne();
+
+            for (int i = 0; i < tableLayoutPanel1.RowCount; i++)
+            {
+                tableLayoutPanel1.Controls[i].Size = new System.Drawing.Size(100, listTailles[i]);
+                tableLayoutPanel1.Controls.Add(new Button(), 0, i);
+            }
+        }
+
+        // RowCount = nombre de ligne de la table
+
     }
 }
